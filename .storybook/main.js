42feb436762e4b32cdf588e72fdbc04f05257fe6
“yksandeep” <yksandeep08+github@gmail.com>
2022-12-05 15:27:19 +0530

    storybook include src/**/*.stories.(js|jsx|ts|tsx)

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,8 +4,7 @@ module.exports = {
   stories: [
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/components/**/*.stories.(js|jsx|ts|tsx)',
-    '../src/shared/**/*.stories.(js|jsx|ts|tsx)',
+    '../src/**/*.stories.(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
